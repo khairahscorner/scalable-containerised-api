907ce94bf96e14256d34c4d45a7d615c711a9009
Airah Yusuff <airahyusuff@gmail.com>
2025-02-02 13:25:03 +0000

    attempt to use remote backend to manage state fiels

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,12 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket = "devops-challenge-tf-state-files"
+    key    = "files/terraform.tfstate"
+    region = "eu-west-2"
+  }
 }
 
 provider "aws" {
