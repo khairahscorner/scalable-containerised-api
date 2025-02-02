cd310db8f481f3f8d5817ad4f21b2ddda0d8d3f2
Airah Yusuff <airahyusuff@gmail.com>
2025-02-02 14:54:18 +0000

    correctly export module output to root

--- terraform/modules/aws_environment/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-availability_zones = ["eu-west-2a", "eu-west-2b"]
-cidr_block         = "10.0.0.0/16"
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
+output "repository_url" {
+  value = module.aws_environment.repository_url
+}
+
 output "load_balancer_url" {
   value = module.ecs_setup.load_balancer_dns
 }
