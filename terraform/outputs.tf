7aa3853b464cdc12aba6c456427d3d78c500bcd3
Airah Yusuff <airahyusuff@gmail.com>
2025-02-02 17:59:52 +0000

    fix: gateway url not exported

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "repository_url" {
 output "load_balancer_url" {
   value = module.ecs_setup.load_balancer_dns
 }
+
+output "api_gateway_url" {
+  value = module.gateway_setup.api_gateway_url
+}
